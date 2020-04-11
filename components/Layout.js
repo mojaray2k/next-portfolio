c44d6f3b00ea666398d16ee0ec5a966b1d84078f
Amen Ra <garagedev@amenra.local>
2020-04-11 03:11:19 -0400

    added NProgress to codebase and used Router events

--- components/Layout.js
+++ components/Layout.js
@@ -1,24 +1,43 @@
 import Link from "next/link";
+import Head from "next/head";
+import Router from "next/router";
+import NProgress from "nprogress";
+
+Router.onRouteChangeStart = (url) => {
+  console.log(url);
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => NProgress.done();
+Router.onRouteChangeError = () => NProgress.done();
 
 export default ({ children, title }) => (
-  <div className="root">
-    <header>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      {"     "}
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-      {"     "}
-      <Link href="hireme">
-        <a>Hire Me</a>
-      </Link>
-    </header>
-    <h1>{title}</h1>
-    {children}
-    <footer>&copy; {new Date().getFullYear()}</footer>
-    <style jsx>{`
+  <>
+    <Head>
+      <title>Next Portfolio</title>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+      />
+    </Head>
+    <div className="root">
+      <header>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        {"     "}
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        {"     "}
+        <Link href="hireme">
+          <a>Hire Me</a>
+        </Link>
+      </header>
+      <h1>{title}</h1>
+      {children}
+      <footer>&copy; {new Date().getFullYear()}</footer>
+      <style jsx>{`
       .root {
         display: flex;
         justify-content: center;
@@ -50,12 +69,13 @@ export default ({ children, title }) => (
       }
     `}</style>
 
-    <style global jsx>{`
-      body {
-        margin: 0;
-        font-size: 110%;
-        background: #f0f0f0;
-      }
-    `}</style>
-  </div>
+      <style global jsx>{`
+        body {
+          margin: 0;
+          font-size: 110%;
+          background: #f0f0f0;
+        }
+      `}</style>
+    </div>
+  </>
 );
