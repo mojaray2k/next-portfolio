364d31598aa51d20b2d61c6db9cbc3cb8e66b93e
Amen Ra <garagedev@amenra.local>
2020-04-11 02:41:04 -0400

    adding default layout for all pages plus jss

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default () => (
+  <Layout title="About">
+    <p>Javascript programmer</p>
+    <img
+      src="static/javascript-logo.png"
+      alt="Javascript Logo"
+      height="200px"
+    />
+  </Layout>
+);
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
-const Index = () => <div>Welcome to the hompage</div>;
+import Layout from "../components/Layout";
+
+const Index = () => (
+  <Layout title="Home">
+    <p>Welcome to the hompage</p>
+  </Layout>
+);
 
 export default Index;
