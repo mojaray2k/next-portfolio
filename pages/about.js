48a0bd8abb3834c9d1e83aec58f62fc549c964f9
Amen Ra <garagedev@amenra.local>
2020-04-11 15:54:53 -0400

    adding refactoring changes

--- pages/about.js
+++ pages/about.js
@@ -4,26 +4,12 @@ import Error from "./_error";
 import { Component } from "react";
 
 export default class About extends Component {
-  // state = {
-  //   user: null,
-  // };
-
   static async getInitialProps() {
     const res = await fetch("https://api.github.com/users/mojaray2k");
     const statusCode = res.status > 200 ? res.status : false;
     const data = await res.json();
     return { user: data, statusCode };
   }
-
-  // componentDidMount() {
-  //   fetch("https://api.github.com/users/mojaray2k")
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       this.setState({
-  //         user: data,
-  //       });
-  //     });
-  // }
 
   render() {
     const { user, statusCode } = this.props;
@@ -33,8 +19,6 @@ export default class About extends Component {
     }
     return (
       <Layout title="About">
-        {/* {JSON.stringify(this.state.user)} */}
-        {/* {JSON.stringify(this.props.user)} */}
         <p>{user.name}</p>
         <img src={user.avatar_url} alt={user.name} height="200px" />
       </Layout>
--- pages/post.js
+++ pages/post.js
@@ -3,7 +3,7 @@ import { withRouter } from "next/router";
 
 const Post = ({ router }) => (
   <Layout title={router.query.title}>
-    <p>
+    <p style={{ width: "88vw" }}>
       Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolores neque
       corporis maiores odit veniam nemo assumenda natus. Rem ipsum quod tempore
       laudantium qui rerum. Quibusdam rerum fuga eligendi temporibus a.
