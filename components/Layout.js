6c28fbbdcf98eb1e3a85a850f85734226e0cb987
Amen Ra <garagedev@amenra.local>
2020-04-11 03:26:49 -0400

    adding _document.js file to persist global next parameters

--- components/Layout.js
+++ components/Layout.js
@@ -15,10 +15,6 @@ export default ({ children, title }) => (
   <>
     <Head>
       <title>Next Portfolio</title>
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
-      />
     </Head>
     <div className="root">
       <header>
---
+++ pages/_document.js
@@ -0,0 +1,39 @@
+import Document, { Head, Main, NextScript } from "next/document";
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <meta
+            name="description"
+            content="A site for my programming portfolio"
+          />
+          <meta charset="utf-8" />
+          <meta name="robots" content="noindex, nofollow" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+        <style global jsx>{`
+          body {
+            font-family: "Roboto", sans-serif;
+          }
+        `}</style>
+      </html>
+    );
+  }
+}
